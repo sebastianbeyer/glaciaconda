09f286491a3c0757e4327f710abb3543a304f308
Sebastian <sebi@riseup.net>
2022-05-25 11:49:01 +0200

    Change time and time_bnds for climate forcing
    Now we use the middle of a month for the the time
    and the first and last day as bounds. This is now using a 360 day
    calendar to make this easy. Before that, I used 365 day calendar and the
    time was always the first day of the month, but this did not always work
    because months had different number of days

--- workflow/scripts/prepare_CESM_atmo.py
+++ workflow/scripts/prepare_CESM_atmo.py
@@ -29,12 +29,6 @@ def set_climatology_time(file):
     @TODO: use with for dataset
     @TODO: middle of month?
     """
-    secPerYear = 365.00 * 24 * 3600
-    secPerMonth = secPerYear / 12
-    timeMonths = np.arange(12)
-    timeSecs = timeMonths * secPerMonth
-    boundsMonths = np.array([[start, start + 1] for start in timeMonths])
-    boundsSecs = boundsMonths * secPerMonth
 
     rootgrp = Dataset(file, "r+")
     if "nv" not in rootgrp.dimensions:
@@ -42,18 +36,19 @@ def set_climatology_time(file):
         rootgrp.createDimension("nv", 2)
 
     nc_time = rootgrp.variables['time']
-    nc_time[:] = timeSecs
+    nc_time[:] = [15, 45, 75, 105, 135, 165, 195, 225, 255, 285, 315, 345]
 
     if "time_bnds" not in rootgrp.variables:
         print("creating time bounds var")
         nc_time_bounds = rootgrp.createVariable(
             "time_bnds", "f8", ("time", "nv"))
     nc_time_bounds = rootgrp.variables['time_bnds']
-    nc_time_bounds[:] = boundsSecs
+    nc_time_bounds[:] = [[0, 30], [30, 60], [60, 90], [90, 120], [120, 150], [150, 180],
+                         [180, 210], [210, 240], [240, 270], [270, 300], [300, 330], [330, 360]]
     print("done")
     nc_time.bounds = 'time_bnds'
-    nc_time.units = 'seconds since 1-1-1'
-    nc_time.calendar = '365_day'
+    nc_time.units = 'days since 1-1-1'
+    nc_time.calendar = '360_day'
 
     rootgrp.close()
 
