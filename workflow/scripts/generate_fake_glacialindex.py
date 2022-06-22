2dc39c5c9a3e0f65e8c472ca393c519f2167c18d
Sebastian <sebi@riseup.net>
2022-06-22 11:33:19 +0200

    make the fake glacial index longer

--- workflow/scripts/generate_fake_glacialindex.py
+++ workflow/scripts/generate_fake_glacialindex.py
@@ -25,6 +25,19 @@ def generate_fake_index():
         [7, 8],
         [8, 9],
         [9, 10],
+        [10, 11],
+        [11, 12],
+        [12, 13],
+        [13, 14],
+        [14, 15],
+        [15, 16],
+        [16, 17],
+        [17, 18],
+        [18, 19],
+        [19, 20],
+        [20, 21],
+        [21, 22],
+        [22, 23],
     ]
     time_bnds = np.array(time_bnds)
     time_bnds_secs = time_bnds * secPerYear
